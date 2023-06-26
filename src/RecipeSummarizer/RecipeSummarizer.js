c0c31d5a759bf8c61ee75c450c8dee2e46892459
Avinash Tingre <tingre.avinash@gmail.com>
2023-06-27 02:51:53 +0530

    UI changes

--- src/RecipeSummarizer/RecipeSummarizer.js
+++ src/RecipeSummarizer/RecipeSummarizer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import cheerio from 'cheerio';
+import './RecipeSummarizer.css';
 
 function RecipeSummarizer() {
     const [url, setUrl] = useState('');
@@ -11,6 +12,7 @@ function RecipeSummarizer() {
 
     const fetchHtmlContent = async () => {
         try {
+            console.log("------------>>>")
             const response = await fetch(url);
             const data = await response.text();
 
@@ -26,7 +28,7 @@ function RecipeSummarizer() {
             const recipeObject = schema['@graph'].find(obj => obj['@type'] === 'Recipe');
 
             // Print the recipe object
-            console.log(recipeObject);
+            console.log("Recipe: {}", recipeObject);
 
             setHtmlContent(recipeObject);
 
@@ -36,31 +38,59 @@ function RecipeSummarizer() {
     };
 
 
-
-    const RecipeStep = ({ step, key }) => {
-        return (
-            <p>{step.text}</p>
-        );
-    };
-
     const RecipeInstructions = ({ instructions }) => {
-        console.log(instructions);
+
         return (
             <div>
-                {instructions.map((section, index) => (
+                {instructions.map((instruction, index) => (
                     <div key={index}>
-                        <h2>{section.name}</h2>
-                        <ol>
-                            {section.itemListElement.map((step, i) => (
-                                <li><RecipeStep key={i} step={step} /></li>
-                            ))}
-                        </ol>
+                        {instruction["@type"] === "HowToSection" ? (
+                            <>
+                                <h2>{instruction.name}</h2>
+                                {instruction.itemListElement.map((step, index) => (
+                                    <li key={index}>{step.text}</li>
+                                ))}
+                            </>
+                        ) : (
+                            <li key={index}>{instruction.text}</li>
+                        )}
                     </div>
                 ))}
             </div>
         );
     };
 
+
+
+    const formatDuration = (duration) => {
+        const regex = /PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/;
+        const match = duration.match(regex);
+
+        const hours = parseInt(match[1]) || 0;
+        const minutes = parseInt(match[2]) || 0;
+        const seconds = parseInt(match[3]) || 0;
+
+        return `${hours > 0 ? hours + ' hours ' : ''}${minutes > 0 ? minutes + ' minutes ' : ''}${seconds > 0 ? seconds + ' seconds' : ''}`;
+    };
+
+    const RecipeDetails = ({ recipe }) => {
+        // Get the last image URL
+        const lastImage = recipe.image && recipe.image.length > 0 ? recipe.image[0] : '';
+      
+        return (
+          <div>
+            {/* ...other code */}
+            {lastImage && <img src={lastImage} className='img-fluid' alt="Recipe" />}
+            {/* ...other code */}
+          </div>
+        );
+      };
+
+      const htmlDecode = (input) => {
+        const doc = new DOMParser().parseFromString(input, "text/html");
+        return doc.documentElement.textContent;
+      };
+      
 
     return (
         <div>
@@ -69,8 +99,8 @@ function RecipeSummarizer() {
                 <div className="container-fluid">
                     <a className="navbar-brand">Recipe Declutter</a>
                     <form className="d-flex" role="search">
-                        <input placeholder="Paste a recipe URL" ariaLabel="Paste a recipe URL" className="form-control me-2" type="Search" id="urlInput" value={url} onChange={handleUrlChange} autocomplete="on" autocorrect="on" />
-                        <button className="btn btn-outline-success" type="submit" onClick={fetchHtmlContent}>submit</button>
+                        <input placeholder="Paste a recipe URL" aria-label="Paste a recipe URL" className="form-control me-2" type="Search" id="urlInput" value={url} onChange={handleUrlChange} autoComplete="on" autoCorrect="on" />
+                        <button className="btn btn-outline-success" type="button" onClick={fetchHtmlContent}>submit</button>
                     </form>
                 </div>
             </nav>
@@ -78,8 +108,11 @@ function RecipeSummarizer() {
 
 
                 <h1>{htmlContent?.name}</h1>
-                <p>{htmlContent?.description}</p>
-                <p>Serving: {htmlContent?.recipeYield}</p>
+                <RecipeDetails recipe={htmlContent} />
+                <p>{htmlContent?.description && htmlDecode(htmlContent?.description)}</p>
+                <p>Serving: {htmlContent?.recipeYield?.map((recipeYield, index) => (
+                    <li key={index}>{recipeYield}</li>
+                ))}</p>
 
                 <h2>Ingredients</h2>
                 <ul>
@@ -90,9 +123,19 @@ function RecipeSummarizer() {
                 <div className=''>
                     <h2>Directions</h2>
                     {htmlContent?.recipeInstructions && (
-                        <RecipeInstructions instructions={htmlContent.recipeInstructions} />
+                        <ol> <RecipeInstructions instructions={htmlContent.recipeInstructions} /> </ol>
                     )}
                 </div>
+                <h2>Preparation Time</h2>
+                <p>{htmlContent?.prepTime && formatDuration(htmlContent?.prepTime)}</p>
+
+                <h2>Cooking Time</h2>
+                <p>{htmlContent?.cookTime && formatDuration(htmlContent?.cookTime)}</p>
+
+                <h2>Total Time</h2>
+                <p>{htmlContent?.totalTime && formatDuration(htmlContent?.totalTime)}</p>
+
+                From: <a target={htmlContent?.mainEntityOfPage}>{htmlContent?.mainEntityOfPage}</a>
             </div>
         </div>
 
